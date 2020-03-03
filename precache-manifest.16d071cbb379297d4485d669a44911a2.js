8150e3c9611f72ce2b65be81068850e158f7e21d
AhmadButt00 <ahmadbutt00@hotmail.com>
2020-03-03 10:58:58 +0500

    Updates

---
+++ precache-manifest.16d071cbb379297d4485d669a44911a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "618e7a422487929601e517dd35afc497",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "fa52d1165bf663092166",
+    "url": "/Monster-Rolodex/static/css/main.393b1cf8.chunk.css"
+  },
+  {
+    "revision": "9146ece56f9ab9286075",
+    "url": "/Monster-Rolodex/static/js/2.85a2fa3b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Monster-Rolodex/static/js/2.85a2fa3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa52d1165bf663092166",
+    "url": "/Monster-Rolodex/static/js/main.9e2d4ed4.chunk.js"
+  },
+  {
+    "revision": "1eab5797a9674ded8b05",
+    "url": "/Monster-Rolodex/static/js/runtime-main.ec79a5a4.js"
+  }
+]);
